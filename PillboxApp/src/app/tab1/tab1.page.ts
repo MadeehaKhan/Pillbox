96302d6f5b73e509d232a2acc5a27cade1b38277
Cesar Antonio Santana Penner <santanca@mcmaster.ca>
2019-02-25 13:32:45 -0500

    #32 pushing and poping from Druglist and Taken list

--- PillboxApp/src/app/tab1/tab1.page.ts
+++ PillboxApp/src/app/tab1/tab1.page.ts
@@ -13,12 +13,32 @@ export class Tab1Page {
     alert("Details!");
   }
 
-  public form = [
-    { val: 'Pepperoni', time: '9:30', isChecked: true },
+  public drugsList = [
+    { val: 'Pepperoni', time: '9:30', isChecked: false },
     { val: 'Sausage', time: '11:00' , isChecked: false },
     { val: 'Mushroom', time: '6:00', isChecked: false }
   ];
 
+  public takenDrugsList = [
+    { val: 'Dog', time: '9:30', isChecked: true },
+  ];
 
+  public updateDrugLists(){
+      this.drugsList.forEach(drug => {
+        if(drug.isChecked === true){
+          var index = this.drugsList.findIndex( i=> i.val == drug.val && i.time == drug.time);
+          this.drugsList.splice(index, 1);
+          this.takenDrugsList.push(drug);
+        }
+      });
+
+      this.takenDrugsList.forEach(drug => {
+        if(drug.isChecked === false){
+          var index = this.takenDrugsList.findIndex( i=> i.val == drug.val && i.time == drug.time);
+          this.takenDrugsList.splice(index, 1);
+          this.drugsList.push(drug);
+        }
+      });
+  }
 
 }
